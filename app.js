a9b20a9d7ba39b9f589190207ac3ef19e4bfcd74
butter <kmj52781@naver.com>
2023-10-13 23:54:57 +0900

    evnet object와 prevntDefault함수

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const userName = loginInput.value;
-        console.log(userName);
+function onLoginSubmit(event) { //event object는 기본적으로 preventDefault함수를 갖음.
+    event.preventDefault(); //브라우저가 기본적으로 수행하는 동작(기본 행동)을 막음
+    console.log(loginInput.value); //예시로, from을 submit 했을 때 브라우저가 새로고침 하는 것(기본 행동)등을 막음
+   
  }
 
+loginForm.addEventListener("submit", onLoginSubmit); //submit할때 onLoginSubmit 함수를 호출한다는 의미
 
-loginButton.addEventListener("click", onLoginBtnClick);
