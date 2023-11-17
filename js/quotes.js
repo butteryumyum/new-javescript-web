9d0fa606c80a78f06e5fcf18c7d3ce70efd4b8c9
butter <kmj52781@naver.com>
2023-11-18 00:11:58 +0900

    2023 11 17 qouotes

--- js/quotes.js
+++ js/quotes.js
@@ -5,47 +5,46 @@ const quotes = [
     },
     {
         quote: "그거 먹고 탑으로 와",
-        anthor: "장경환(MaRin)",
+        author: "장경환(MaRin)",
     },
     {
         quote: "D점멸 쓰면 실버로 간다.",
-        anthor: "이상혁(Faker)",
+        author: "이상혁(Faker)",
     },
     {
         quote: "난 죽는 걸 아는데도 정글때문에 죽어야 한다. 난 다 생각하면서 게임을 한다.",
-        anthor: "강승록(TheShy)",
+        author: "강승록(TheShy)",
     },
     {
         quote: "여러분들이 지면 여러분들의 잘못이 아닙니다. 그건 정글 잘못입니다.",
-        anthor: "김동하(Khan)",
+        author: "김동하(Khan)",
     },
     {
         quote: "상대는 지금 4킬을 따 놓고도 내가 무서워서 갱을 부르고 있다.",
-        anthor: "김대호(cvMax)",
+        author: "김대호(cvMax)",
     },
     {
         quote: "4 Chinese Can't Win",
-        anthor: "김동하(Khan)",
+        author: "김동하(Khan)",
     },
     {
         quote: "미드랑 바텀이 터지는건 내 알바가 아니다.",
-        anthor: "최현준(Doran)",
+        author: "최현준(Doran)",
     },
     {
         quote: "탑은 3분 서렌 기능이 필요하다.",
-        anthor: "정상길(Dopa)",
+        author: "정상길(Dopa)",
     },
     {
         quote: "방광암 박주헌",
-        anthor: "박진호(ji-no-park)",
+        author: "박진호(ji-no-park)",
     },
 
 ];
 
 const quote = document.querySelector("#quote span:first-child");
-const anthor = document.querySelector("#quote span:last-child");
-
+const author = document.querySelector("#quote span:last-child");
 const toDaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = toDaysQuote.quote;
-author.innerText = toDaysQuote.authoer;
+author.innerText = toDaysQuote.author;
