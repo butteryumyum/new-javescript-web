8de47717ec97e4d6a45fad053f2563e2569920a7
butter <kmj52781@naver.com>
2023-11-23 00:31:20 +0900

    2023 11 23 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function handleToDoList(event) {
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = "";
-    
+function paintToDo(newTodo) {
+	    const li = document.createElement("li"); //element (li)를 생성함
+	    const span = document.createElement("span"); //elemnt (span)을 생성
+    li.appendChild(span); //li안에 span을 종속
+    span.innerText = newTodo; //text 교체
+    toDoList.appendChild(li); //toDoList안에 li를 종속
 }
 
-toDoForm.addEventListener("submit", handleToDoList);
+function handleToDoList(event) { //submit event(새로고침)
+    event.preventDefault(); //기본동작 막음
+	    const newTodo = toDoInput.value;
+	    toDoInput.value = "";
+	    paintToDo(newTodo); //paintToDo 호출
+}
+	toDoForm.addEventListener("submit", handleToDoList);
