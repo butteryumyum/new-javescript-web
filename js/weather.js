4b13b8fc1afaef26c621377e924dfb7ca35de272
butter <kmj52781@naver.com>
2023-12-06 23:49:39 +0900

    2023 12 06 weather API

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,13 @@
+
+const API_KEY = "3e0609d2c193f0e190798ac796422777";
+
 function onGeoOk(position) //위치를 잘 받았을때
 {   
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live it", lat, lng);
+    const lon = position.coords.longitude;
+    console.log("You live it", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url);
 }
 function onGeoError() //위치를 받지 못하고 에러가 났을때
 {
